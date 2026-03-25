348fae8d35b6268f246022b01b2df48761b2ea58
Vincent Leclair <vleclair90@gmail.com>
2026-03-25 19:38:25 -0400

    Auto-commit: Version update (v0.6.0.0325)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Windows;
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.2.0325")]
-[assembly: AssemblyFileVersion("0.5.2.0325")]
-[assembly: AssemblyInformationalVersion("0.5.2.0325")]
-[assembly: AssemblyCopyright("Copyright (c) NoID Softwork 2026-2026")]
+[assembly: AssemblyVersion("0.6.0.0325")]
+[assembly: AssemblyFileVersion("0.6.0.0325")]
+[assembly: AssemblyInformationalVersion("0.6.0.0325")]
+[assembly: AssemblyCopyright("Copyright (c) NoID Softwork 2020-2026")]
 [assembly: AssemblyCompany("NoID Softwork")]
 [assembly: AssemblyTitle("PaDDY")]
 [assembly: AssemblyProduct("PaDDY")]
@@ -17,6 +17,7 @@ using System.Reflection;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
 
 
 
