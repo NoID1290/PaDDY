61a3219de9705c71b0e12c2e3079a411a836700a
Vincent Leclair <vleclair90@gmail.com>
2026-05-05 16:08:18 -0400

    Testing SIGN_2 (v1.1.3.0505)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.1.3.0505")]
 [assembly: AssemblyFileVersion("1.1.3.0505")]
-[assembly: AssemblyInformationalVersion("1.1.3.0505-Pre-release_1")]
+[assembly: AssemblyInformationalVersion("1.1.3.0505-Pre-release_2")]
 [assembly: AssemblyCopyright("Copyright (c) NoID Softwork 2020-2026")]
 [assembly: AssemblyCompany("NoID Softwork")]
 [assembly: AssemblyTitle("PaDDY")]
@@ -17,6 +17,7 @@ using System.Reflection;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
 
 
 
