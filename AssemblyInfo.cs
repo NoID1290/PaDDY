d6d66d2cb5f1f5580238432220f8514027bea4cf
Vincent Leclair <vleclair90@gmail.com>
2026-06-28 15:12:37 -0400

    Registering PADBACK File Association (v1.5.0.0628)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.5.0.0628")]
 [assembly: AssemblyFileVersion("1.5.0.0628")]
-[assembly: AssemblyInformationalVersion("1.5.0.0628-Pre-release_1")]
+[assembly: AssemblyInformationalVersion("1.5.0.0628")]
 [assembly: AssemblyCopyright("Copyright (c) NoID Softwork 2020-2026")]
 [assembly: AssemblyCompany("NoID Softwork")]
 [assembly: AssemblyTitle("PaDDY")]
@@ -17,6 +17,7 @@ using System.Reflection;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
 
 
 
