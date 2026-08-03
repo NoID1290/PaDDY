cd08cd968cec89c748d455f786348cf693bceb04
Vincent Leclair <vleclair90@gmail.com>
2026-08-03 16:31:43 -0400

    Auto-commit: Version update (v2.0.1.0803)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Windows;
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.0802")]
-[assembly: AssemblyFileVersion("2.0.0.0802")]
-[assembly: AssemblyInformationalVersion("2.0.0.0802")]
+[assembly: AssemblyVersion("2.0.1.0803")]
+[assembly: AssemblyFileVersion("2.0.1.0803")]
+[assembly: AssemblyInformationalVersion("2.0.1.0803")]
 [assembly: AssemblyCopyright("Copyright (c) NoID Softwork 2020-2026")]
 [assembly: AssemblyCompany("NoID Softwork")]
 [assembly: AssemblyTitle("PaDDY")]
@@ -17,6 +17,7 @@ using System.Reflection;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
 
 
 
